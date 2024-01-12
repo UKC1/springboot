ecb35c64fc99a019db38ca168980a75078ad6efa
kangmin <lnewgatel@gmail.com>
2024-01-12 18:00:15 +0900

    Ex03_jspMybatis

---
+++ Ex03_jspMybatis/src/main/java/com/study/dto/Board.java
@@ -0,0 +1,35 @@
+package com.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Data안에 들어있는 5가지 어노테이션
+//@Getter
+//@Setter
+//@RequiredArgsConstructor
+//@ToString
+//@EqualsAndHashCode
+
+// 위의 어노테이션을 하나로 만든 어노테이션
+// 생성자 1개만 생성됨
+// - @NonNull이 붙어있는 필드만 매개변수로한 생성자
+// - @NonNull이 하나도 없을 떄는 매개변수가 없는 생성자
+
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Data
+public class Board {
+	@NonNull // null값을 가질 수 없다 null이면 exception 발생
+	private int no;
+	private String title;
+	private String writer;
+	@NonNull
+	private String content;
+}
