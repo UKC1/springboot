245a399a4efc1eef2857ad6e10ea070d2cf90746
kangmin <lnewgatel@gmail.com>
2024-01-18 15:34:02 +0900

    Memberjpa

--- Ex06_Memberjpa01/src/main/java/com/study/Ex06Memberjpa01Application.java
+++ Ex06_Memberjpa01/src/main/java/com/study/Ex06Memberjpa01Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex06Memberjpa01Application {
+public class Ex06MemberJpa01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ex06Memberjpa01Application.class, args);
+		SpringApplication.run(Ex06MemberJpa01Application.class, args);
 	}
 
 }
--- Ex06_Memberjpa01/src/test/java/com/study/Ex06Memberjpa01ApplicationTests.java
+++ Ex06_Memberjpa01/src/test/java/com/study/Ex06Memberjpa01ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ex06Memberjpa01ApplicationTests {
+class Ex06MemberJpa01ApplicationTests {
 
 	@Test
 	void contextLoads() {
