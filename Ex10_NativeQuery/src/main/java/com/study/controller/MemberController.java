75d8e88425e4931d8c9fafc02b6c804a68261fc2
kangmin <lnewgatel@gmail.com>
2024-01-22 11:53:24 +0900

    Update MemberController.java

--- Ex10_NativeQuery/src/main/java/com/study/controller/MemberController.java
+++ Ex10_NativeQuery/src/main/java/com/study/controller/MemberController.java
@@ -18,6 +18,7 @@ import com.study.service.MemberService;
 
 @Controller
 public class MemberController {
+	
 	@Autowired
 	MemberService memberService;
 	
@@ -27,38 +28,39 @@ public class MemberController {
 	}
 	
 	@GetMapping("/selectByNameLike1")
-	public String selectMembers1(@RequestParam("name") String search, Model model) {	
-		String name = search + "%";
-		model.addAttribute("members", memberService.selectMembers1(name));
-		return "select_name_list1";
+	public String selectMembers1(@RequestParam("name") String search, Model model) {
+		 String name = search + "%";
+		 model.addAttribute("members", memberService.selectMembers1(name));
+		 return "select_name_list1";
 	}
 	
 	@GetMapping("/selectByNameLike2")
-	public String selectMembers2(@RequestParam("name") String search, Model model) {	
-		String name = search + "%";
-		Sort sort = Sort.by(Sort.Order.asc("id"));
-		model.addAttribute("members", memberService.selectMembers2(name, sort));
-		return "select_name_list2";
+	public String selectMembers2(@RequestParam("name") String search, Model model) {
+		 String name = search + "%";
+		 Sort sort = Sort.by(Sort.Order.asc("id"));
+		 model.addAttribute("members", memberService.selectMembers2(name, sort));
+		 return "select_name_list1";
 	}
-	
+
 	@GetMapping("/selectByNameLike3")
 	public String selectMembers3(@RequestParam("name") String search,
 									@RequestParam("page") int page,
-									Model model) {	
+									Model model)
+	{
 		String name = search + "%";
 		Sort sort = Sort.by(Sort.Order.desc("name"));
-		// JPA에서 paging은 1페이지가 0부터 시작
-		int nPage = page - 1;
 		
-		// 1page당 10개씩 
+		// JPA에서 paging은 1페이는 0부터 시작
+		int nPage = page - 1;
+							// 1page당 10개씩
 		Pageable pageable = PageRequest.ofSize(10).withPage(nPage).withSort(sort);
 		
 		Page<Member> result = memberService.selectMembers3(name, pageable);
-		List<Member> content = result.getContent(); 	// 실제 객체가 담긴 List (content)
-		long totalElements = result.getTotalElements(); // 토탈 content 개수
-		int totalPages = result.getTotalPages(); 		// 토탈 페이지수
-		int size = result.getSize();					// 1page당 보여줄 갯수
-		int pageNumber = result.getNumber() + 1; 		// 현재페이지 0부터 시작
+		List<Member> content = result.getContent();			// 실제 객체가 담긴 List (content)
+		long totalElements = result.getTotalElements();		// 토탈 content개수
+		int totalPages = result.getTotalPages();			// 토탈 페이지수
+		int size = result.getSize();						// 1page당 보여줄 갯수
+		int pageNumber = result.getNumber() + 1;			// 현재페이지 0부터 시작
 		int numberOfElements = result.getNumberOfElements(); // 현재페이지의 content개수
 		
 		model.addAttribute("members", content);
@@ -70,12 +72,11 @@ public class MemberController {
 		
 		return "select_name_list2";
 	}
-	
+
 	@GetMapping("/selectByNameLike4")
-	public String selectMembers4(@RequestParam("name") String search, Model model) {	
-		String name = search + "%";
-		model.addAttribute("members", memberService.selectMembers4(name));
-		return "select_name_list1";
+	public String selectMembers4(@RequestParam("name") String search, Model model) {
+		 String name = search + "%";
+		 model.addAttribute("members", memberService.selectMembers4(name));
+		 return "select_name_list1";
 	}
-	
 }
